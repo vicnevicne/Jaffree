fb5fd405f585875778ee0cf26316a74adb1da09e
Denis Kokorin <kokorin86@gmail.com>
2020-05-11 12:30:10 +0300

    Fix checkstyle: FormatParser, Format, Frame

--- src/main/java/com/github/kokorin/jaffree/ffprobe/Format.java
+++ src/main/java/com/github/kokorin/jaffree/ffprobe/Format.java
@@ -22,59 +22,117 @@ import com.github.kokorin.jaffree.ffprobe.data.DTag;
 
 import java.util.List;
 
+/**
+ * Format description.
+ */
 public class Format {
     private final DSection section;
 
-    public Format(DSection section) {
+    /**
+     * Creates {@link Format} description based on provided data sections.
+     *
+     * @param section data section
+     */
+    public Format(final DSection section) {
         this.section = section;
     }
 
+    /**
+     * Returns data section which holds all the data provided by ffprobe for current Format.
+     * <p>
+     * Use this method if you have to access properties which are not accessible through
+     * other getters in this class.
+     *
+     * @return data section
+     */
     public DSection getSection() {
         return section;
     }
 
+    /**
+     * Returns tags for this Format.
+     *
+     * @return tags
+     */
     public List<Tag> getTag() {
         return section.getTag("TAG").getValues(DTag.TAG_CONVERTER);
     }
 
+    /**
+     * @return file name
+     */
     public String getFilename() {
         return section.getString("filename");
     }
 
+    /**
+     * @return number of streams
+     */
     public int getNbStreams() {
         return section.getInteger("nb_streams");
     }
 
+    /**
+     * @return number of programs
+     */
     public int getNbPrograms() {
         return section.getInteger("nb_programs");
     }
 
+    /**
+     * @return format name
+     */
     public String getFormatName() {
         return section.getString("format_name");
     }
 
+    /**
+     * @return format long name
+     */
     public String getFormatLongName() {
         return section.getString("format_long_name");
     }
 
+    /**
+     * @return media start time in seconds
+     */
     // TODO: getter with TimeUnit?
     public Float getStartTime() {
         return section.getFloat("start_time");
     }
 
+    /**
+     * @return media duration in seconds
+     */
     // TODO: getter with TimeUnit?
     public Float getDuration() {
         return section.getFloat("duration");
     }
 
+    /**
+     * @return media size in bytes
+     */
     public Long getSize() {
         return section.getLong("size");
     }
 
+    /**
+     * @return media bitrate in bits per second
+     */
     public Long getBitRate() {
         return section.getLong("bit_rate");
     }
 
+    /**
+     * Returns detected format score.
+     * <p>
+     * FFprobe assigns corresponding probe score to each known format during input analysis.
+     * The return format by ffprobe is the one with highest score.
+     * <p>
+     * Higher values (up to 100) mean higher probability of correct format detection.
+     *
+     * @return format probe score
+     */
     public Integer getProbeScore() {
         return section.getInteger("probe_score");
     }
